27ddda68c022f314d556e43e20b3afd7f70c155f
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2015-12-22 11:20:54 +0000

    Add simple content item route...
    Makes a simple request for a single content item.

---
+++ spec/controllers/multipart_contents_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe MultipartContentsController, type: :controller do
+  describe "GET show" do
+    context "for an existing content item" do
+      before do
+        content_store_has_item("/vat-rates")
+      end
+
+      it "renders the content item" do
+        get :show, slug: "vat-rates"
+
+        expect(response).to be_successful
+      end
+    end
+
+    context "for an invalid path" do
+      before do
+        content_store_does_not_have_item("/cat-rates")
+      end
+
+      it "responds with 404" do
+        get :show, slug: "cat-rates"
+
+        expect(response.status).to eq(404)
+      end
+    end
+  end
+end
